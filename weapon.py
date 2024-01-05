93d39e792a315068601d629c26eff83d8e99e1af
Mohammed El Barhichi <mohammed.elbarhichi@gmail.com>
2024-01-05 10:23:15 +0100

    done

--- weapon.py
+++ weapon.py
@@ -3,22 +3,22 @@ from exceptions import NoAmmunitionError
 
 class Weapon:
     def __init__(self, ammunitions: int, range: int):
-        self._ammunitions = ammunitions
-        self._range = range
+        self.ammunitions = ammunitions
+        self.range = range
 
     def fire_at(self, x, y, z):
-        if self._ammunitions == 0:
+        if self.ammunitions == 0:
             raise NoAmmunitionError("Vous n'avez plus de munitions !")
 
         self.check_target_position(x, y, z)
 
-        self._ammunitions = self._ammunitions - 1
+        self.ammunitions = self.ammunitions - 1
 
     def get_ammunitions(self):
-        return self._ammunitions
+        return self.ammunitions
 
     def get_range(self):
-        return self._range
+        return self.range
 
     def check_target_position(self, x, y, z):
         raise NotImplementedError()
